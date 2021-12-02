3f6a71c486fe48f42e0245f9853918481f61418a
SebaWnek <47484373+SebaWnek@users.noreply.github.com>
2021-12-02 01:20:37 +0100

    Dodaj pliki projektów.

---
+++ Jammer/AD9833.cs
@@ -0,0 +1,17 @@
+﻿using Meadow.Hardware;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Jammer
+{
+    class AD9833
+    {
+        private SpiPeripheral Ad9833;
+
+        public AD9833(ISpiBus bus, IDigitalOutputPort port)
+        {
+            Ad9833 = new SpiPeripheral(bus, port, 16, 16, ChipSelectMode.ActiveLow);
+        }
+    }
+}
